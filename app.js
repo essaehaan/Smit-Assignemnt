539c101c72b381f11bd4200d75aa7c2d7976fb3e
Essa Ehaan <essa.nagar2@gmail.com>
2024-08-31 15:45:43 +0500

    Js Assignament

---
+++ app.js
@@ -0,0 +1,9 @@
+let firstName, lastName;
+firstName = "Muhammad"
+lastName = "Essa"
+var address = "main street"
+address= "first street"
+
+console.log(firstName);
+console.log(lastName);
+console.log(address);
