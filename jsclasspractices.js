0dca2c7c7cc5b8daa424f41e79eff6da45a426a8
Essa Ehaan <94886958+essaehaan@users.noreply.github.com>
2024-09-08 17:15:54 +0500

    Create jsclasspractices.js

---
+++ jsclasspractices.js
@@ -0,0 +1,19 @@
+//Class Practices 
+
+ let cities = ['Karachi', 'Haiderabad', 'peshawar', 'gilgit']
+ console.log(cities)
+ cities.push("Moro")
+ console.log(cities)
+ cities.pop()
+ console.log(cities)
+ cities.shift()
+ console.log(cities)
+ cities.unshift("Nagar Valley", "Baltistan")
+ console.log(cities)
+ cities.splice(3, 2, "Chilas")
+ console.log(cities)
+ let maincities = cities.slice(0, 2)
+ console.log(maincities)
+
+
+
